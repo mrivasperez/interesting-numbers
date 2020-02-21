2936952d46faf599da87d625363799749ce98808
Miguel Rivas Perez <35717991+mrivasperez@users.noreply.github.com>
2020-02-21 12:46:37 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,41 @@ let numberInput = document.querySelector('#numberInput')
 
 
 // Event listeners
-numberInput.addEventListener('input', getFactAJAX);
+numberInput.addEventListener('input', getFactFetch);
 
-function getFactAJAX() {
+// USING FETCH API
+function getFactFetch () {
     let number = numberInput.value;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://numbersapi.com/${number}`);
-
-    xhr.onload = function(){
-        if(this.status == 200 && number != '') {
+    fetch(`http://numbersapi.com/${number}`)
+    .then(response => response.text())
+    .then(data => {
+        if(number != '') {
             fact.classList.remove('hide-fact');
-            factText.innerText = this.responseText;
-            console.log('number added')
+            factText.innerText = data;
+        } else if (numberInput.value == 0) {
+            fact.classList.add('hide-fact');
         }
+    })
+    .catch(error => console.log('error'));
 
-    }
-    xhr.send();
 }
+
+// GETTING INFO VIA AJAX ---
+// function getFactAJAX() {
+//     let number = numberInput.value;
+
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', `http://numbersapi.com/${number}`);
+
+//     xhr.onload = function(){
+//         if(this.status == 200 && number != '') {
+//             fact.classList.remove('hide-fact');
+//             factText.innerText = this.responseText;
+//             console.log('number added')
+//         }
+
+//     }
+//     xhr.send();
+// }
 
 // 
